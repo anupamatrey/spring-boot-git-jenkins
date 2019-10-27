54fe255d2866375a830d6d92a3179113559abfb0
Anupam <anupam.attrey@gmail.com>
2019-10-27 18:14:03 -0400

    Add Controller

---
+++ src/main/java/com/anupam/jenkins/SpringController.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.anupam.jenkins;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Anupam Sharma
+ *
+ */
+@RestController
+public class SpringController {
+	
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
